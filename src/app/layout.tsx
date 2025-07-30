bfd4a9fe627c4dcd95c27c4218e6221024622af2
isixe <isixe@outlook.com>
2025-07-30 22:24:33 +0800

    🦄 refactor(app): update page metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,45 +1,50 @@
-import type { Metadata } from "next";
 import { Toaster } from "@/components/ui/sonner";
 import "@/styles/globals.css";
 import { ThemeProvider } from "@/components/provider/theme-provider";
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
 
-export const metadata: Metadata = {
-	title: "Image Dash - Reverse Image & Text Search",
-	description:
-		"Find any image fast with Image Dash's powerful reverse image search. Upload an image, paste a URL, or describe an image to search across Google, Yandex, Bing, and more.",
-	keywords: [
-		"reverse image search",
-		"image finder",
-		"search by image",
-		"photo search",
-		"Image Dash",
-		"text to image search",
-	],
-	openGraph: {
+import type { Metadata } from "next";
+import { headers } from "next/headers";
+
+export async function generateMetadata(): Promise<Metadata> {
+	const headersList = await headers();
+	const protocol = headersList.get("x-forwarded-proto");
+	const host = headersList.get("host");
+	const url = `${protocol}://${host}`;
+
+	return {
+		metadataBase: new URL(url),
 		title: "Image Dash - Reverse Image & Text Search",
-		description: "The quickest way to find any image on the web.",
-		url: "https://image-dash.itea.dev/",
-		siteName: "Image Dash",
-		images: [
-			{
-				url: "https://placehold.co/1200x630.png",
-				width: 1200,
-				height: 630,
-				alt: "Image Dash helping find an image on the web",
-			},
+		keywords: [
+			"reverse image search",
+			"image finder",
+			"search by image",
+			"photo search",
+			"Image Dash",
+			"text to image search",
 		],
-		locale: "en_US",
-		type: "website",
-	},
-	twitter: {
-		card: "summary_large_image",
-		title: "Image Dash - Reverse Image & Text Search",
-		description: "The quickest way to find any image on the web.",
-		images: ["https://placehold.co/1200x630.png"],
-	},
-};
+		description:
+			"Find any image fast with Image Dash's powerful reverse image search. Upload an image, paste a URL, or describe an image to search across Google, Yandex, Bing, and more.",
+		alternates: {
+			canonical: url,
+		},
+		openGraph: {
+			title: "Image Dash - Reverse Image & Text Search",
+			description: "The quickest way to find any image on the web.",
+			url: new URL(url),
+			siteName: "Image Dash",
+			images: [
+				{
+					url: "/preview.png",
+					alt: "Image Dash helping find an image on the web",
+				},
+			],
+			locale: "en_US",
+			type: "website",
+		},
+	};
+}
 
 export default function RootLayout({
 	children,
